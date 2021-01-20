08d62b6179d22f24b3cf0804a01c2947114004ed
Bastian Carvajal <bastian.carvajal@2brains.cl>
2021-01-20 12:17:11 -0300

    RestServer

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,16 @@
 
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de datos
+let urlDB;
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else {
+    urlDB = 'mongodb+srv://admin_node:trini1023@cluster0.b6kqb.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
 require('./config/config');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,34 +11,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuarios', function (req, res) {
-    res.json('Get Usuario')
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB,
+    {
+                useNewUrlParser: true,
+                useCreateIndex: true
+            },
+    (err, res) => {
+
+    if (err) throw new Error();
+    console.log('Base de datos online.');
 });
-
-app.post('/usuarios', function (req, res) {
-
-    let body = req.body;
-    if(body.nombre === undefined){
-        res.status(400).json({
-           ok: false,
-           mensaje: 'El nombre es necesario.'
-        });
-    }else {
-        res.json({persona: body});
-    }
-
-});
-
-app.put('/usuarios/:id', function (req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuarios', function (req, res) {
-    res.json('Delete Usuario')
-});
-
 app.listen(process.env.PORT, () => console.log(`Escuchando en el puerto ${process.env.PORT}`));
